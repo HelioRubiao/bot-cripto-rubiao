74e98b5296f2254248e114cbd9c958b27be539b8
HelioRubiao <41922146+HelioRubiao@users.noreply.github.com>
2026-03-23 00:05:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def enviar_noticia():
 
     print("Enviando notícia...")
     enviar_telegram(msg)
-    enviar_telegram("🚀 TESTE GRUPO")
+    enviar_telegram("🚀 TESTE FINAL GRUPO")
 "\n\n💎 Em breve: sinais completos no grupo VIP"
 enviar_noticia()
 
