9c4638237beb960768a55258e3b585fc23e1d6e6
HelioRubiao <41922146+HelioRubiao@users.noreply.github.com>
2026-04-04 17:37:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -111,8 +111,8 @@ while True:
 
             preco_binance = get_price_binance(MAPA_BINANCE[coin])
 
-            if rsi < 55 and preco < media:
-                if preco_binance and abs(preco - preco_binance) / preco < 0.02:
+            if rsi < 35 and preco < media:
+                if preco_binance
                     if coin not in ultimo_sinal or ultimo_sinal[coin] != "COMPRA":
                         enviar_free(
                             f"🟢 COMPRA\n"
